e3d8eb0c72adad8971877c2aaeb5748906c8e1f6
Bohdan Lukasevych <>
2022-04-28 11:46:53 +0300

    trip commit

--- force-app/main/default/lwc/tripHandler/tripHandler.js
+++ force-app/main/default/lwc/tripHandler/tripHandler.js
@@ -1,3 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire,api } from 'lwc';
+import getTrips from '@salesforce/apex/TripHandler.getTrips';
+import TRIPCODE from '@salesforce/schema/Trip__c.Name';
+import ARRIVALCITY from '@salesforce/schema/Trip__c.Arrival_City__c';
+import DEPARTUNECITY from '@salesforce/schema/Trip__c.Departune_City__c';
+import DRIVER from '@salesforce/schema/Trip__c.Driver__c';
+import PRICE from '@salesforce/schema/Trip__c.Price__c';
 
-export default class TripHandler extends LightningElement {}
+const columns = [
+    {label:"Trip Code",fieldName: TRIPCODE.fieldApiName },
+    {label:"Arrival City",fieldName: ARRIVALCITY.fieldApiName },
+    {label:"Departune City",fieldName: DEPARTUNECITY.fieldApiName },
+    {label:"Driver",fieldName: DRIVER.fieldApiName },
+    {label:"Price",fieldName: PRICE.fieldApiName }
+];
+
+export default class TripHandler extends LightningElement {
+    @api myTrip;
+    columns = columns;
+
+    @wire(getTrips)
+    wireTrips({data,error}){
+        if(data){
+            this.myTrip = data;
+        } else if(error){
+            console.log(error);
+        } 
+    }
+}
